15c27672c55eec2885ce95d94cc93bba70c1574e
unknown <archi.tibil@gmail.com>
2020-08-21 17:08:09 +0200

    first lesson

--- script.js
+++ script.js
@@ -2,17 +2,21 @@
 
 const numberOfFilms = prompt('How much films are you see?', '');
 
-const lastFilm = prompt('last seed film?', '');
-const Rate = prompt('how much u rate it?', '');
 
 const personalMoviedDB = {
     count: numberOfFilms,
-    movies: {
-        yourmovie : lastFilm, Rate
-        },
+    movies: {},
     actors: {},
     genres: [],
     privat: false
 };
  
+const a = prompt('Last film if u seed?', ''),
+      b = prompt('How are u rate it?', ''),
+      c = prompt('Last film if u seed?', ''),
+      d = prompt('How are u rate it?', '');
+
+      personalMoviedDB.movies[a] = b;
+      personalMoviedDB.movies[c] = d;
+
 console.log(personalMoviedDB);
