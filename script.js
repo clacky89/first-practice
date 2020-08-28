09af9c2a058c7bfee4d56d10b36e832ebdcc4b52
Artur <archi.tibili@gmail.com>
2020-08-29 00:21:06 +0200

    Function practice

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfFilms = prompt('How much films are you see?', '');
+let numberOfFilms;
+
+function start () {
+  numberOfFilms = +prompt('How much films are you see?', '');
+
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('How much films are you see?', '');    
+  }
+}
+
+start();
 
 
 const personalMoviedDB = {
@@ -11,30 +21,48 @@ const personalMoviedDB = {
     privat: false
 };
 
-for(let i = 0; i < 2; i++){
-  const a = prompt('Last film if u seed?', ''),
-        b = prompt('How are u rate it?', '');
-
-        personalMoviedDB.movies[a] = b;
-        
-  if (a != null && b != null && a != '' && b != '' && a.length < 50){
-    console.log('done');
-  } else {
-    console.log('error');
-    i--;
+function showMyDB () {
+  if (personalMoviedDB.privat == false){
+    console.log(personalMoviedDB);
   }
 }
+showMyDB ();
 
-if (personalMoviedDB.count < 10){
-  alert ('So low watched films');
-}else if (10 <= personalMoviedDB.count && personalMoviedDB.count < 30){
-  alert ('You are classical viewer');
-}else if (personalMoviedDB.count > 999999){
-  alert ('You are Cineman');
-} else{
-  alert ('Error');
+
+
+function rememberMyFilms() {
+  for(let i = 0; i < 2; i++){
+    const a = prompt('Last film if u seed?', ''),
+          b = prompt('How are u rate it?', '');
+  
+          personalMoviedDB.movies[a] = b;
+          
+    if (a != null && b != null && a != '' && b != '' && a.length < 50){
+      console.log('done');
+    } else {
+      console.log('error');
+      i--;
+    }
+  }
 }
+rememberMyFilms ();
 
-console.log(personalMoviedDB);
+function writeYourGenres (){
+    for (let i = 1; i <= 3; i++){ 
+      personalMoviedDB.genres[i - 1] = prompt(`Your favourite genre of films $ {i}`);
+    }
+}
+writeYourGenres ();
 
-//Нормальная версия
+function detectPersonalLevel() {
+  if (personalMoviedDB.count < 10){
+    alert ('So low watched films');
+  }else if (10 <= personalMoviedDB.count && personalMoviedDB.count <= 30){
+    alert ('You are classical viewer');
+  }else if (personalMoviedDB.count < 999999){
+    alert ('You are Cinefyle');
+  } else{
+    alert ('Error');
+  }
+}
+detectPersonalLevel() ;
