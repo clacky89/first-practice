49024b6e7c25e8a7ba19c12badd387a239dac2a3
Artur <archi.tibili@gmail.com>
2020-09-10 09:00:47 +0200

    transparate all functions in to the object

--- script.js
+++ script.js
@@ -18,51 +18,48 @@ const personalMoviedDB = {
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+
+    
+    checkPrivat: function () {
+      if (personalMoviedDB.privat == false){
+        console.log(personalMoviedDB);
+      }
+    },
+    rememberMyFilms: function () {
+      for(let i = 0; i < 2; i++){
+        const a = prompt('Last film if u seed?', ''),
+              b = prompt('How are u rate it?', '');
+      
+              personalMoviedDB.movies[a] = b;
+              
+        if (a != null && b != null && a != '' && b != '' && a.length < 50){
+          console.log('done');
+        } else {
+          console.log('error');
+          i--;
+        }
+      }
+    },
+    writeYourGenres: function () {
+      for (let i = 1; i <= 3; i++){ 
+        personalMoviedDB.genres[i - 1] = prompt(`Your favourite genre of films $ {i}`);
+      }
+    },
+    detectPersonalLevel: function () {
+      if (personalMoviedDB.count < 10){
+        alert ('So low watched films');
+      }else if (10 <= personalMoviedDB.count && personalMoviedDB.count <= 30){
+        alert ('You are classical viewer');
+      }else if (personalMoviedDB.count < 999999){
+        alert ('You are Cinefyle');
+      } else{
+        alert ('Error');
+      }
+    }
 };
 
-function showMyDB () {
-  if (personalMoviedDB.privat == false){
-    console.log(personalMoviedDB);
-  }
-}
-showMyDB ();
-
-
-
-function rememberMyFilms() {
-  for(let i = 0; i < 2; i++){
-    const a = prompt('Last film if u seed?', ''),
-          b = prompt('How are u rate it?', '');
-  
-          personalMoviedDB.movies[a] = b;
-          
-    if (a != null && b != null && a != '' && b != '' && a.length < 50){
-      console.log('done');
-    } else {
-      console.log('error');
-      i--;
-    }
-  }
-}
-rememberMyFilms ();
-
-function writeYourGenres (){
-    for (let i = 1; i <= 3; i++){ 
-      personalMoviedDB.genres[i - 1] = prompt(`Your favourite genre of films $ {i}`);
-    }
-}
-writeYourGenres ();
-
-function detectPersonalLevel() {
-  if (personalMoviedDB.count < 10){
-    alert ('So low watched films');
-  }else if (10 <= personalMoviedDB.count && personalMoviedDB.count <= 30){
-    alert ('You are classical viewer');
-  }else if (personalMoviedDB.count < 999999){
-    alert ('You are Cinefyle');
-  } else{
-    alert ('Error');
-  }
-}
-detectPersonalLevel() ;
+personalMoviedDB.checkPrivat ();
+personalMoviedDB.rememberMyFilms ();
+personalMoviedDB.writeYourGenres ();
+personalMoviedDB.detectPersonalLevel ();
